e4299c148f334f7d1f0555af28865b5596b4bc78
Jared <jaredjrsong@gmail.com>
2023-07-15 17:43:09 +1000

    synced up beats with notes and opacity

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const canvas = document.querySelector("#canvas");
 const pen = canvas.getContext("2d");
+pen.shadowColor = "white";
+pen.shadowBlur = 100;
+
+const settings = {
+  reverse: false,
+};
 
 const startTime = new Date().getTime();
 
@@ -7,13 +13,13 @@ const anchorAngle1 = 0;
 const anchorAngle2 = (2 * Math.PI) / 3;
 const anchorAngle3 = (4 * Math.PI) / 3;
 
-// const numTriangles = 21;
 const numTriangles = 21;
+// const numTriangles = 1;
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 const anchorRadius = canvas.height * 0.4;
 const circleRadius = anchorRadius * 0.025;
-const musicCircleRadius = circleRadius * 0.35;
+const musicCircleRadius = circleRadius * 0.25;
 let triangles = [];
 
 const center = {
@@ -57,7 +63,7 @@ const draw = () => {
   const currentTime = new Date().getTime();
   let elapsedTime = (currentTime - startTime) / 1000;
 
-  const anchor = drawAnchor(center, elapsedTime);
+  const anchor = drawAnchor(center, elapsedTime, currentTime);
   const x1 = anchor.x1,
     y1 = anchor.y1,
     x2 = anchor.x2,
@@ -66,13 +72,17 @@ const draw = () => {
     y3 = anchor.y3;
 
   for (i = 0; i < numTriangles; i++) {
-    drawInnerTriangles(x1, y1, x2, y2, x3, y3, i, elapsedTime);
+    drawInnerTriangles(x1, y1, x2, y2, x3, y3, i, elapsedTime, currentTime);
     if (currentTime >= triangles[i][3]) {
-      triangles[i][4].play()
-      triangles[i][2] = triangles[i][3]
+      triangles[i][4].play();
+      triangles[i][2] = triangles[i][3];
     }
 
-    triangles[i][3] = calculateNextBeatTime(triangles[i][2], triangles[i][1], triangles[i][0])
+    triangles[i][3] = calculateNextBeatTime(
+      triangles[i][2],
+      triangles[i][1],
+      triangles[i][0]
+    );
   }
 
   requestAnimationFrame(draw);
@@ -80,9 +90,15 @@ const draw = () => {
 draw();
 
 function initNote(index) {
-  const audio = new Audio(`resources/${index + 1}.mp3`)
+  let note;
+  if (settings.reverse) {
+    note = index + 1;
+  } else {
+    note = numTriangles - index;
+  }
+  const audio = new Audio(`resources/${note}.mp3`);
   audio.volume = 0.15;
-  
+
   triangles[i][4] = audio;
 }
 
@@ -92,10 +108,17 @@ function calculateNextBeatTime(lastBeatTime, distance, velocity) {
   return lastBeatTime + (distance / velocity) * 1000;
 }
 
-function drawInnerTriangles(x1, y1, x2, y2, x3, y3, index, elapsedTime) {
-  pen.strokeStyle = "gray";
-  pen.lineWidth = 0.5;
-
+function drawInnerTriangles(
+  x1,
+  y1,
+  x2,
+  y2,
+  x3,
+  y3,
+  index,
+  elapsedTime,
+  currentTime
+) {
   const fraction = index / numTriangles;
   const newX1 = (x2 - x1) * fraction + x1;
   const newY1 = (y2 - y1) * fraction + y1;
@@ -106,7 +129,7 @@ function drawInnerTriangles(x1, y1, x2, y2, x3, y3, index, elapsedTime) {
   const newX3 = (x1 - x3) * fraction + x3;
   const newY3 = (y1 - y3) * fraction + y3;
 
-  drawTriangle(newX1, newY1, newX2, newY2, newX3, newY3);
+  drawTriangle(newX1, newY1, newX2, newY2, newX3, newY3, currentTime, index);
   drawMusicCircles(
     newX1,
     newY1,
@@ -115,34 +138,47 @@ function drawInnerTriangles(x1, y1, x2, y2, x3, y3, index, elapsedTime) {
     newX3,
     newY3,
     elapsedTime,
+    currentTime,
     index
   );
 }
 
-function drawMusicCircles(x1, y1, x2, y2, x3, y3, elapsedTime, index) {
+function drawMusicCircles(x1, y1, x2, y2, x3, y3, elapsedTime, currentTime, index) {
   // draw music circles
-  pen.fillStyle = "purple";
-  pen.lineWidth = 1;
+  pen.fillStyle = "white";
+  pen.lineWidth = 2;
 
+  pen.globalAlpha = calculateOpacity(currentTime, triangles[index][2], 0, 1, 4000);
   const pos1 = getMovingPos(x1, y1, x2, y2, index, elapsedTime);
   pen.beginPath();
   pen.arc(pos1.newX, pos1.newY, musicCircleRadius, 0, 2 * Math.PI);
   pen.fill();
+  pen.globalAlpha = 0.7;
+  pen.strokeStyle = "#7470b6";
+  pen.stroke();
 
+  pen.globalAlpha = calculateOpacity(currentTime, triangles[index][2], 0, 1, 4000);
   const pos2 = getMovingPos(x2, y2, x3, y3, index, elapsedTime);
   pen.beginPath();
   pen.arc(pos2.newX, pos2.newY, musicCircleRadius, 0, 2 * Math.PI);
   pen.fill();
+  pen.globalAlpha = 0.7;
+  pen.strokeStyle = "#7470b6";
+  pen.stroke();
 
+  pen.globalAlpha = calculateOpacity(currentTime, triangles[index][2], 0, 1, 4000);
   const pos3 = getMovingPos(x3, y3, x1, y1, index, elapsedTime);
   pen.beginPath();
   pen.arc(pos3.newX, pos3.newY, musicCircleRadius, 0, 2 * Math.PI);
   pen.fill();
+  pen.globalAlpha = 0.7;
+  pen.strokeStyle = "#7470b6";
+  pen.stroke();
 }
 
 function drawAnchor(center, elapsedTime) {
-  pen.strokeStyle = "blue";
-  pen.lineWidth = 1.5;
+  pen.strokeStyle = "#7470b6";
+  pen.lineWidth = 2;
 
   // draw anchor circle 1
   let angle = anchorAngle1 + elapsedTime / 10;
@@ -174,13 +210,21 @@ function drawAnchor(center, elapsedTime) {
   pen.beginPath();
   pen.arc(center.x, center.y, anchorRadius, 0, 2 * Math.PI);
   pen.stroke();
-
-  // anchor triangle
-  drawTriangle(x1, y1, x2, y2, x3, y3);
   return { x1, y1, x2, y2, x3, y3 };
 }
 
-function drawTriangle(x1, y1, x2, y2, x3, y3) {
+function calculateOpacity(currentTime, lastImpactTime, baseOpacity, maxOpacity, duration) {
+  const timeSinceImpact = currentTime - lastImpactTime,
+    percentage = Math.min(timeSinceImpact / duration, 1),
+    opacityDelta = maxOpacity - baseOpacity;
+  return maxOpacity - opacityDelta * percentage;
+}
+
+function drawTriangle(x1, y1, x2, y2, x3, y3, currentTime, index) {
+  pen.lineWidth = 0.5;
+  pen.globalAlpha = calculateOpacity(currentTime, triangles[index][2], 0, 1, 8000);
+  pen.strokeStyle = "#F5c782";
+
   pen.beginPath();
   pen.moveTo(x1, y1);
   pen.lineTo(x2, y2);
@@ -207,13 +251,18 @@ function initTriangle(x1, y1, x2, y2, index) {
   const cycleDuration = 900; // 900 = 15 mins
   const velocity = (numBounces * maxDistance) / cycleDuration;
 
-  triangles[index] = [velocity, maxDistance, startTime, calculateNextBeatTime(startTime, maxDistance, velocity)];
+  triangles[index] = [
+    velocity,
+    maxDistance,
+    startTime,
+    calculateNextBeatTime(startTime, maxDistance, velocity),
+  ];
 }
 
 function getMovingPos(x1, y1, x2, y2, index, elapsedTime) {
   const velocity = triangles[index][0];
   const maxDistance = triangles[index][1];
-  
+
   let delta_x = x2 - x1;
   let delta_y = y2 - y1;
   const distance = elapsedTime * velocity;
